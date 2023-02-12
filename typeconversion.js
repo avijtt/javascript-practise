4b513d8759484364af1c3146ad19816443419cf5
avijtt <infoavi9090@gmail.com>
2023-02-12 18:45:30 +0545

    initial commit

---
+++ typeconversion.js
@@ -0,0 +1,21 @@
+//typeof is element
+
+//string conversion
+
+let value=true;
+alert(typeof value);  //boolean  automatically change by alert
+
+value=String(value);
+alert(typeof value) //change into string
+
+
+
+// numeric conversion
+
+alert("10"/"2")  //change string to number on division
+
+let value1="123"  //string
+alert(typeof value1) //string
+
+let num=Number(value1);
+alert(typeof num)
