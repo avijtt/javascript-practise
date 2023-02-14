494c0716951dec0a4721ae51d6906fd4e80524af
avijtt <infoavi9090@gmail.com>
2023-02-14 17:03:06 +0545

    new js file

--- javascript/logicalop.js
+++ javascript/logicalop.js
@@ -41,3 +41,63 @@ let minute = 30;
 if (hoUr == 12 && minute == 30) {
   console.log( 'The time is 12:30' );
 }
+
+// AND returns the first falsy value or the last value if none were found.
+
+// if the first operand is truthy,
+// AND returns the second operand:
+alert( 1 && 0 ); // 0
+alert( 1 && 5 ); // 5
+
+// if the first operand is falsy,
+// AND returns it. The second operand is ignored
+alert( null && 5 ); // null
+alert( 0 && "no matter what" ); // 0
+
+
+// The precedence of AND && operator is higher than OR ||.
+
+// NOT(!)
+Result= !okay
+
+
+    //does:
+// Converts the operand to boolean type: true/false.
+// Returns the inverse value.
+alert( !true ); // false
+alert( !0 ); // true
+
+
+
+//tasks
+alert(null || 2 || undefined) //2
+alert( alert(1) || 2 || alert(3) ); //2 alert(1) and alert(3 ) return undefined
+alert(1 && null && 2); //null  null is falsy on check from left to right
+
+alert( null || 2 && 3 || 4 ); 
+//The precedence of AND && is higher than ||, so it executes first.
+//The result of 2 && 3 = 3, so the expression becomes:
+//null || 3 || 4
+//Now the result is the first truthy value: 3.
+
+
+let userName=prompt('who is there?','');
+if(userName=='Admin') {
+    let pass=prompt('password?','')
+    if(pass=='TheMaster'){
+        alert('Welcome Home!');
+    }
+    else if(pass==null || pass==''){
+        alert('cancelled')
+    }
+    else{
+        alert('Wrong password')
+    }
+
+}
+else if(userName==null || userName==''){
+    alert('cancelled');
+}
+else{
+    alert("I don't know you");
+}
