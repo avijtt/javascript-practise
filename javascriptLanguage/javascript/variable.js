1f434a00d328294a528df8e09baa794d8100ca4d
avijtt <infoavi9090@gmail.com>
2023-05-11 08:30:26 +0545

    javascript variable

---
+++ javascriptLanguage/javascript/variable.js
@@ -0,0 +1,31 @@
+//semicolon sometimes breaks the line 
+//Here, ; symbol runs one by one code
+alert("Hello");
+
+[1, 2].forEach(alert);
+///here only one line code work
+alert("Hello")
+
+
+[1, 2].forEach(alert)
+
+//variables
+let message="hello"
+let id=1
+const value=123
+console.log(message,id,value)
+
+let $=11;
+let _=2
+console.log($+_)
+
+//CONST VARIABLE SHOULD BE UPPERCASE
+const ORANGECOLOR="f00";
+const blackColor="#000"
+
+let color=ORANGECOLOR;
+console.log(color)
+
+
+
+
